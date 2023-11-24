18e0dc77bbd85fbf62625eddb811ac485164345e
unknown <tien.manh.do99@gmail.com>
2023-11-24 11:02:08 +0700

    create project

---
+++ Management-Farm/src/main/java/management_farm/ManagementFarmApplication.java
@@ -0,0 +1,13 @@
+package management_farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementFarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementFarmApplication.class, args);
+    }
+
+}
---
+++ Management-Farm/src/test/java/management_farm/ManagementFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package management_farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementFarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
